b6d0cea602b5b835335582758b82912d38f8316d
tdragos <dragostudor96@gmail.com>
2017-09-07 10:50:13 +0300

    spring + mysql kickstarter

---
+++ src/main/java/netgloo/models/UserDao.java
@@ -0,0 +1,40 @@
+package netgloo.models;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * A DAO for the entity User is simply created by extending the CrudRepository
+ * interface provided by spring. The following methods are some of the ones
+ * available from such interface: save, delete, deleteAll, findOne and findAll.
+ * The magic is that such methods must not be implemented, and moreover it is
+ * possible create new query methods working only by defining their signature!
+ *
+ * @author netgloo
+ */
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+    /**
+     * Return the user having the passed email or null if no user is found.
+     *
+     * @param email the user email.
+     */
+    public User findByEmail(String email);
+
+    /**
+     * Return the user having the passed id or null if no user is found.
+     *
+     * @param id the user email.
+     */
+    public User findById(String id);
+
+    /**
+     * Return the user having the passed name or null if no user is found.
+     *
+     * @param name the user email.
+     */
+    public User findBName(String name);
+
+} // class UserDao
